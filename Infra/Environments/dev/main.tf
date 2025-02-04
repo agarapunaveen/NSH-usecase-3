b3b89bf4ea83338c15b6d3bbb22fa0a9023b7fe7
agarapunaveen <67690657+agarapunaveen@users.noreply.github.com>
2025-02-04 13:33:06 +0530

    Update main.tf

--- Infra/Environments/dev/main.tf
+++ Infra/Environments/dev/main.tf
@@ -1,1 +1,17 @@
 
+module "iam" {
+  source = "../../Modules/IAM"
+  
+}
+
+module "ecr" {
+  source    = "../../Modules/ECR"
+}
+
+
+
+module "lamda" {
+  source = "../../Modules/LAMDA"
+  lambda_role_arn = module.iam.lambda_role_arn
+  attach_basic_execution = module.iam.attach_basic_execution
+}
